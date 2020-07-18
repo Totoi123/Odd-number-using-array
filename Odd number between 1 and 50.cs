958f59296afec781826d4c03890578b8f953b28b
Somojit Arambam <42943741+Totoi123@users.noreply.github.com>
2020-07-18 13:35:49 +0530

    Add files via upload

---
+++ Odd number between 1 and 50.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 50;
+            int[] odd = new int[n];
+
+            int i = 0;
+
+            Console.WriteLine("Odd Numbers:");
+            for(i = 1; i<= n; i++)
+            {
+                if(i % 2 != 0)
+                {
+                    Console.Write(odd[i] + i + " ");
+                }
+            }
+            Console.Read();
+        }
+    }
+}
